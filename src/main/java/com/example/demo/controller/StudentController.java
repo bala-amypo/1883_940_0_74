16de7d0022a1ba5e71c891179c53a54c109355d6
bala-amypo <bala@amypo.in>
2025-12-18 10:12:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,57 +1,28 @@
-package com.example.demo.entity;
+package com.example.demo.controller;
 
-import java.time.LocalDate;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Entity
- @Table(name="StudentTable")
-public class StudentController{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message=" Name field cannot be empty")
-    @Size(min=3,max=20,message="The user name must be of min 3 and max 20 character")
-    private String name;
-    @Email(message="Invalid email id")
-    private String email;
-    //@NotNull
-    //@Size(min,max)(to reccomend the use to the give no of characters)
-    //@Min
-    //@Max
-    //@Pattern(reg)(used for phone numbers)
-    //@Positive(used in bank record detailling)
-    public Long getId() {
-        return id;
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/postdata")
+    public ResponseEntity<Student> postdata(
+            @Valid @RequestBody Student student) {
+
+        Student savedStudent = studentService.saveStudent(student);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public Student(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        
-    }
-    public Student() {
-    } 
 }
